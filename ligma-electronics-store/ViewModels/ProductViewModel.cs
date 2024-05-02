5670f23f0023e3672f8724dc931416329650213b
Vairis Stals <ovairis@gmail.com>
2024-05-02 18:26:25 +0300

    Implementing back-end code for product CRUD functionality

---
+++ ligma-electronics-store/ViewModels/ProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectronicsStore.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ShoppingCartId { get; set; }
+
+        [RegularExpression("([a-zA-Z0-9 .&'-]+)", ErrorMessage = "The field Name should only include letters and number.")]
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Required]
+        public decimal Price { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
