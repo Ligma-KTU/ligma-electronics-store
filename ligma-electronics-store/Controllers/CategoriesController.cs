f27d2bfdb90e0cccadf93e297b19b271baddb08b
raivov10@gmail.com <raivov10@gmail.com>
2024-05-02 19:02:09 +0300

    CRUD for categories

---
+++ ligma-electronics-store/Controllers/CategoriesController.cs
@@ -0,0 +1,93 @@
+﻿using ligma_electronics_store.Models;
+using ligma_electronics_store.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ligma_electronics_store.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult Create([Bind("Name, ImageUrl")] CategoryViewModel categoryViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(categoryViewModel);
+
+            var category = new Category
+            {
+                Name = categoryViewModel.Name,
+                ImageUrl = categoryViewModel.ImageUrl
+            };
+
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            var editViewModel = new CategoryViewModel
+            {
+                Id = category.Id,
+                Name = category.Name,
+                ImageUrl = category.ImageUrl
+            };
+
+            return View(editViewModel);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult Edit([Bind("Id, Name, ImageUrl")] CategoryViewModel editViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(editViewModel);
+
+            var category = _context.Categories.FirstOrDefault(c => c.Id == editViewModel.Id);
+
+            category.Name = editViewModel.Name;
+            category.ImageUrl = editViewModel.ImageUrl;
+
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
