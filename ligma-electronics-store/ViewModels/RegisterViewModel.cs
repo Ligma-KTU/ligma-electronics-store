098a3a1ed924a8e9f3e76dfada5480a6fd3a2ea5
Vairis Stals <ovairis@gmail.com>
2024-05-03 00:43:19 +0300

    ES 18

---
+++ ligma-electronics-store/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ligma_electronics_store.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "The Username field is required.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The Password field is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
