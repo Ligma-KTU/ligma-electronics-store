440a56e49e0635421de69283a61947554ac49d79
raivov10@gmail.com <raivov10@gmail.com>
2024-05-03 01:32:38 +0300

    ES 27

---
+++ ligma-electronics-store/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ligma_electronics_store.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        [RegularExpression("([a-zA-Z0-9 .&'-]+)", ErrorMessage = "The field Name should only include letters and number.")]
+        [Required]
+        public string Name { get; set; }
+
+        [Url(ErrorMessage = "Invalid URL format.")]
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
